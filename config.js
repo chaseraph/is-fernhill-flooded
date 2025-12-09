df64bb123e5e21cd9f3245623c13438502042f00
chaseraph <48416209+chaseraph@users.noreply.github.com>
2025-12-08 17:35:39 -0800

    Change isFlooded to true in config.js
    testing

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 const CONFIG = {
     // CHANGE THIS TO true FOR YES, false FOR NO
-    isFlooded: false, 
+    isFlooded: true, 
     
     // Your Traffic Cam URLs go here
     cam1: "https://tripcheck.com/RoadCams/cams/Fern%20Hill%20Rd%20at%20Taylor%20Way_pid3782.JPG",
     cam2: "https://tripcheck.com/RoadCams/cams/Fern%20Hill%20Rd%20at%20Taylor%20Way_pid4523.jpg",
     cam3: "https://tripcheck.com/RoadCams/cams/Fern%20Hill%20Rd%20at%20Geiger%20Rd_pid4521.jpg"
+
 };
